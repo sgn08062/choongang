4d98b3f5bde2b24517024b5fb06a81b9a38d160f
coding404 <yopy0817@gmail.com>
2025-09-15 15:36:36 +0900

    01-servlet-jsp-mvc-template

--- src/main/java/com/example/jsp/chap01/HelloServlet.java
+++ src/main/java/com/example/jsp/chap01/HelloServlet.java
@@ -8,28 +8,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(urlPatterns = "/hello")
+// TODO: /hello 요청을 처리하는 서블릿
 public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        //요청
-        //System.out.println("서블릿 동작함");
-        String uri = request.getRequestURI();
-        System.out.println("요청주소:" + uri);
-        String param = request.getParameter("age");
-        System.out.println("요청파라미터:" + param);
-
-        //응답
-        response.setContentType("text/html;charset=UTF-8;");
-        PrintWriter out = response.getWriter();
-        out.println("<div style='color:red;'>");
-        out.println("<h1>Hello World!</h1>");
-        out.println("</div>");
-
-
-
+        //TODO: 서블릿 코드 작성
     }
 
     
--- src/main/java/com/example/jsp/chap02/ByeServlet.java
+++ src/main/java/com/example/jsp/chap02/ByeServlet.java
@@ -7,23 +7,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/bye")
+//TODO: /bye 요청을 처리하는 서블릿
 public class ByeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
-        // 요청 정보 출력
-        String uri = request.getRequestURI();
-        System.out.println("ByeServlet 요청주소: " + uri);
-        
-        // 리다이렉트 이동 - 주소가 바뀜
-        //response.sendRedirect("/chap02/bye_ok.jsp");
-        
-        // 포워딩 - 주소 안바뀜
-        // 포워딩은 화면에 데이터를 전달할 때 사용
-        request.setAttribute("message", "서블릿에서 전달받은 메시지입니다!");
-        request.getRequestDispatcher("/chap02/bye_ok.jsp").forward(request, response);
+        //TODO: 서블릿 코드 작성
+
+        //리다이렉트 vs forward
     }
 
 }
